c962e180aaefc9a1a9050458655d69d59540b2a6
David Cerbino <dcerbino@gmail.com>
2023-03-30 14:15:43 -0300

    added coverage

--- gateways/splitwise_test.go
+++ gateways/splitwise_test.go
@@ -20,3 +20,25 @@ func TestOpen(t *testing.T) {
 	assert.Equal(t, ctx, result.getCtx())
 	assert.Equal(t, log, result.getClient().Logger)
 }
+
+func TestMainCategoryCache(t *testing.T) {
+	assert.Equal(t, true, len(mainCategoryCache) >= 7)
+}
+
+func TestCurenciesCache(t *testing.T) {
+	assert.Equal(t, true, len(curenciesCache) >= 148)
+}
+
+func TestClose(t *testing.T) {
+	token := "test"
+	ctx := context.Background()
+	log := log.New(os.Stdout, "Splitwise LOG: ", log.Lshortfile)
+
+	conn := Open(token, ctx, log)
+
+	executor := conn.GetCurecies()
+
+	assert.Equal(t, false, executor.isClose())
+	executor.Close()
+	assert.Equal(t, true, executor.isClose())
+}
